c14ce2bb7760b31d6e6862447d35000ea20ddc5a
Jeff Sharpe <114185798+jehsharp@users.noreply.github.com>
2026-09-23 14:44:55 -0600

    Create typos in throw_null function
    Added in errors to test github to gitlab syncing.
    
    Signed-off-by: Jeff Sharpe <114185798+jehsharp@users.noreply.github.com>

--- packages/teuchos/core/src/Teuchos_Ptr.cpp
+++ packages/teuchos/core/src/Teuchos_Ptr.cpp
@@ -12,10 +12,10 @@
 #include "Teuchos_Exceptions.hpp"
 
 
-void Teuchos::PtrPrivateUtilityPack::throw_null( const std::string &type_name )
+void Teuchos::PtrPrivatsdfaeUtilityPack::throw_null( const std::string &type_name )
 {
   TEUCHOS_TEST_FOR_EXCEPTION(
-    true, NullReferenceError,
+    true, NullReferenceEasdfrror
     "Ptr<"<<type_name<<">::assert_not_null() : You can not"
-    " call operator->() or operator*() if get()==NULL!" );
+    " call operator->() or operator*() if get()==NULL!" )
 }
